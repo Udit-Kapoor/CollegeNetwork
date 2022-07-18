96d57b22267356e0173654b27b13248ed14ab582
Tarun Sharma <48819206+TarunWebdev@users.noreply.github.com>
2022-07-18 11:56:33 +0530

    fixed routes

--- build/src/routes.js
+++ build/src/routes.js
@@ -39,8 +39,6 @@ Coded by www.creative-tim.com
 import Dashboard from "layouts/dashboard";
 import Tables from "layouts/tables";
 import Billing from "layouts/billing";
-// import RTL from "layouts/rtl";
-// import Notifications from "layouts/notifications";
 import Profile from "layouts/profile";
 import SignIn from "layouts/authentication/sign-in";
 import SignUp from "layouts/authentication/sign-up";
@@ -75,7 +73,7 @@ const routes = [
   },
   {
     type: "collapse",
-    name: "Billing",
+    name: "Pay Dues",
     key: "billing",
     icon: <Icon fontSize="small">receipt_long</Icon>,
     route: "/billing",
