e85c2f8f48b48b70b2a8539320f220d362f80701
Udit-Kapoor <udit.dude01@gmail.com>
2022-07-30 01:28:33 +0530

    feat: add fetch Food Balance

---
+++ build/src/api/fetchUserFood.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+export const getFoodBal = async (address) =>  {
+
+    try {
+
+        const response = await Promise.all([
+            axios.get(`https://api.ghostnet.tzkt.io/v1/bigmaps/156137/keys/{ "nat": "0", "address": "${address}" }`),
+            axios.get(`https://api.ghostnet.tzkt.io/v1/bigmaps/156137/keys/{ "nat": "1", "address": "${address}" }`),
+            axios.get(`https://api.ghostnet.tzkt.io/v1/bigmaps/156137/keys/{ "nat": "2", "address": "${address}" }`),
+            axios.get(`https://api.ghostnet.tzkt.io/v1/bigmaps/156137/keys/{ "nat": "3", "address": "${address}" }`)
+        ]);
+
+        value = {};
+
+        for(var i = 0 ; i< response.length ; i++){
+            if(response[i].status === 200){
+                value[i] = response[i].data.value;
+            }
+            else{
+                value[i] = 0
+            }
+        }
+
+        console.log(value);
+        return{
+            sucess : true,
+            value : value
+        };
+
+        
+    } catch (error) {
+        console.log(error);
+        return{
+            sucess:false,
+            value : {}
+        };
+        
+    }
+
+}
